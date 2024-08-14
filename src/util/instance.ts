6978bb50c896d1270bc6217ac7728d5d0b8abdad
kkt9102 <kkt9102@gmail.com>
2024-08-14 15:37:23 +0900

    feat : api start url change

--- src/util/instance.ts
+++ src/util/instance.ts
@@ -5,7 +5,8 @@ const instance = axios.create({
   //   process.env.NODE_ENV === "production"
   //     ? "https://kkt9102.com:8086/"
   //     : process.env.PUBLIC_URL,
-  baseURL: "https://kkt9102.com:8086/",
+  // baseURL: "https://kkt9102.com:8086/",
+  baseURL: "https://ktkim9102.com:8086/",
   withCredentials: false,
   timeout: 1000,
 });
